69e5bc4316a2a04ecf3340e28748e04f609c254f
KristianColville1 <kristiancolville1@gmail.com>
2024-02-24 17:11:42 +0000

    Added getter method for returning the number of staff members

--- src/ca2/Company.java
+++ src/ca2/Company.java
@@ -7,45 +7,55 @@ package ca2;
 import java.util.ArrayList;
 
 /**
- * 
+ *
  * @author kristian
  */
 public class Company {
+
     // instance fields
     private String companyName;
     private ArrayList<Employee> staff;
-    
+
     /**
      * Default Constructor for Company
      */
-    public Company(){
+    public Company() {
         this.companyName = "CompanyName";
         this.staff = new ArrayList<Employee>();
     }
-    
+
     /**
      * Overloaded Constructor for Company
+     *
      * @param name of the company
      */
-    public Company(String name){
+    public Company(String name) {
         this.companyName = name;
         this.staff = new ArrayList<Employee>();
     }
-    
+
     /**
      * ------------------------------------------------Getter Methods
      */
-    
+    /**
+     * Getter method for retrieving the number of employees in the staff array
+     * list
+     *
+     * @return the number of staff members
+     */
+    public int getStaffNumber() {
+        return staff.size();
+    }
+
     /**
      * ------------------------------------------------Utility Methods
      */
     /**
-     * Utility method for adding a new employee object to the staff array
-     * list.
-     * 
+     * Utility method for adding a new employee object to the staff array list.
+     *
      * @param emp is the employee object being added
      */
-    public void addNewStaff(Employee emp){
+    public void addNewStaff(Employee emp) {
         staff.add(emp);
     }
 }
