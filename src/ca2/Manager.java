bda246ce71bd607eba797e1d92ccad397ae2d47b
KristianColville1 <kristiancolville1@gmail.com>
2024-02-24 17:35:06 +0000

    Added setter for setting new password of the manager

--- src/ca2/Manager.java
+++ src/ca2/Manager.java
@@ -34,14 +34,25 @@ public class Manager extends Employee {
     public String getPassword() {
         return password;
     }
+
     /**
      * ------------------------------------------------Setter Methods
      */
     /**
      * Setter method for modifying the username of the manager
+     *
      * @param username to be used as the new username
      */
-    public void setUsername(String username){
+    public void setUsername(String username) {
         this.username = username;
+    }
+
+    /**
+     * Setter method for modifying the password of the manager
+     *
+     * @param password to be used as the new password
+     */
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
