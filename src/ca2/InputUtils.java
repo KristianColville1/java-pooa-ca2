7e661cd2631a258ced54842fc4ee01a5e6d38c58
KristianColville1 <kristiancolville1@gmail.com>
2024-03-03 14:41:19 +0000

    Added method to extract user input from terminal in static context

--- src/ca2/InputUtils.java
+++ src/ca2/InputUtils.java
@@ -4,10 +4,31 @@
  */
 package ca2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
+ * Utility class providing static methods to use around user input.
  *
  * @author kristian
  */
 public class InputUtils {
-    
+
+    /**
+     * Static method to return user input. Uses Buffered reader and
+     * InputStreamReader to gather the text entered into the terminal by the
+     * user.
+     *
+     * @return user input
+     */
+    public static String getUserInput() {
+        String input = "";
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            input = reader.readLine();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return input;
+    }
 }
